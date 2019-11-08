421d3556f21231b07502afae5fd767b13eab8972
Rachel Waag <36991961+raywaag@users.noreply.github.com>
2019-11-08 17:10:03 -0600

    Over Haul

---
+++ js/main.js
@@ -0,0 +1,128 @@
+AOS.init({
+  duration: 800,
+  easing: "slide"
+});
+
+$(document).ready(function($) {
+  "use strict";
+
+  $(window).stellar({
+    responsive: false,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: "scroll"
+  });
+
+  // Scrollax
+  $.Scrollax();
+
+  var carousel = function() {
+    $(".home-slider").owlCarousel({
+      loop: true,
+      autoplay: true,
+      margin: 0,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: false,
+      dots: false,
+      autoplayHoverPause: false,
+      items: 1,
+      navText: [
+        "<span class='ion-md-arrow-back'></span>",
+        "<span class='ion-chevron-right'></span>"
+      ],
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        600: {
+          items: 1,
+          nav: false
+        },
+        1000: {
+          items: 1,
+          nav: false
+        }
+      }
+    });
+  };
+  carousel();
+
+  var burgerMenu = function() {
+    $(".js-nav-toggle").on("click", function(event) {
+      event.preventDefault();
+      var $this = $(this);
+      if ($("body").hasClass("menu-show")) {
+        $("body").removeClass("menu-show");
+        $("#main-nav > .js-nav-toggle").removeClass("show");
+      } else {
+        $("body").addClass("menu-show");
+        setTimeout(function() {
+          $("#main-nav > .js-nav-toggle").addClass("show");
+        }, 600);
+      }
+    });
+  };
+  burgerMenu();
+
+  var contentWayPoint = function() {
+    var i = 0;
+    $(".animate").waypoint(
+      function(direction) {
+        if (direction === "down" && !$(this.element).hasClass("animated")) {
+          i++;
+
+          $(this.element).addClass("item-animate");
+          setTimeout(function() {
+            $("body .animate.item-animate").each(function(k) {
+              var el = $(this);
+              setTimeout(
+                function() {
+                  var effect = el.data("animate-effect");
+                  if (effect === "fadeIn") {
+                    el.addClass("fadeIn animated");
+                  } else if (effect === "fadeInLeft") {
+                    el.addClass("fadeInLeft animated");
+                  } else if (effect === "fadeInRight") {
+                    el.addClass("fadeInRight animated");
+                  } else {
+                    el.addClass("fadeInUp animated");
+                  }
+                  el.removeClass("item-animate");
+                },
+                k * 50,
+                "easeInOutExpo"
+              );
+            });
+          }, 100);
+        }
+      },
+      { offset: "95%" }
+    );
+  };
+  contentWayPoint();
+
+  // magnific popup
+  $(".image-popup").magnificPopup({
+    type: "image",
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+});
